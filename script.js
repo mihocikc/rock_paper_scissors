7bd054dc61c7ecd34efe8055e2ba2a23a7ff5664
mihocikc <mihocikc@gmail.com>
2022-11-07 17:03:43 -0500

    Create player selection prompt and convert string to all lowercase.

--- script.js
+++ script.js
@@ -3,12 +3,18 @@
 const getComputerChoice = function () {
   let randomNumber = Math.trunc(Math.random() * 3);
   if (randomNumber === 2) {
-    console.log("Rock");
+    console.log("rock");
   } else if (randomNumber === 1) {
-    console.log("Paper");
+    console.log("paper");
   } else if (randomNumber === 0) {
-    console.log("Scissors");
+    console.log("scissors");
   }
 };
 
-console.log(getComputerChoice());
+function playRound(playerChoice, computerChoice) {
+  playerChoice = prompt("Choose rock, paper, or scissors");
+  return playerChoice.toLowerCase();
+}
+
+//console.log(getComputerChoice());
+console.log(playRound());
