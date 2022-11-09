a1822b8a8f5f7cbf5d69711716dab66b3a5f8975
mihocikc <mihocikc@gmail.com>
2022-11-09 14:53:50 -0500

    Create a round of rock, paper, scissors with else if. Used displayMessage functions to reduce string repetition

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // test: console.log('Hello World!');
 
+//GETTING COMPUTER CHOICE
 function getComputerChoice() {
   let randomNumber = Math.trunc(Math.random() * 3);
   if (randomNumber === 2) {
@@ -10,5 +11,37 @@ function getComputerChoice() {
     return "scissors";
   }
 }
-console.log(getComputerChoice());
-//console.log(playRound(playerSelection, getComputerChoice()));
+const computerSelection = getComputerChoice();
+//GETTING PLAYER CHOICE
+let playerSelection = prompt("Rock, paper, or scissors?");
+playerSelection = playerSelection.toLowerCase();
+
+//Display message functions to reduce repetition
+function displayMessageWin(player, computer) {
+  console.log(`You win, ${player} beats ${computer}`);
+}
+function displayMessageLoss(player, computer) {
+  console.log(`You lose, ${computer} beats ${player}`);
+}
+
+//PLAYING A ROUND
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === "paper" && computerSelection === "rock") {
+    return displayMessageWin(playerSelection, computerSelection);
+  } else if (playerSelection === "paper" && computerSelection === "scissors") {
+    return displayMessageLoss(playerSelection, computerSelection);
+  } else if (playerSelection === "rock" && computerSelection === "scissors") {
+    return displayMessageWin(playerSelection, computerSelection);
+  } else if (playerSelection === "rock" && computerSelection === "paper") {
+    return displayMessageLoss(playerSelection, computerSelection);
+  } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    return displayMessageWin(playerSelection, computerSelection);
+  } else if (playerSelection === "scissors" && computerSelection === "rock") {
+    return displayMessageLoss(playerSelection, computerSelection);
+  } else {
+    return `Tie! You both chose ${playerSelection}`;
+  }
+}
+
+//console.log(playerSelection, computerSelection);
+console.log(playRound(playerSelection, computerSelection));
