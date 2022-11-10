01c60193c293df85cfd271cd2ec214e69f3bedea
mihocikc <mihocikc@gmail.com>
2022-11-09 21:00:16 -0500

    Create function to recieve user input, change to lowercase, and respond appropriately for invalid inputs

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 function getComputerChoice() {
-  let choice = Math.trunc(Math.random() * 3);
-  if (choice === 0) {
+  let computerChoice = Math.trunc(Math.random() * 3);
+  if (computerChoice === 0) {
     return "rock";
-  } else if (choice === 1) {
+  } else if (computerChoice === 1) {
     return "paper";
-  } else if (choice === 2) {
+  } else if (computerChoice === 2) {
     return "scissors";
+  }
+  console.log(`Computer threw ${computerChoice}`);
+}
+
+function getPlayerChoice() {
+  let playerChoice = prompt("Choose rock, paper, or scissors").toLowerCase();
+  if (playerChoice === "rock") {
+    return `You threw ${playerChoice}`;
+  } else if (playerChoice === "paper") {
+    return `You threw ${playerChoice}`;
+  } else if (playerChoice === "scissors") {
+    return `You threw ${playerChoice}`;
+  } else {
+    return `Invalid response. Pick rock, paper, or scissors`;
   }
 }
 
 console.log(getComputerChoice());
+console.log(getPlayerChoice());
